49b97e4135f41bcd94a4c7aa9fcc0685e32366d6
Ruslana17 <ruslana.fankovskaya@gmail.com>
2023-09-11 13:31:37 +0200

    update

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
 const soundElements = document.querySelectorAll('.list_birds_name');
 
-console.log(birdsName);
+/*console.log(birdsName);*/
+
+const playPauseButton = document.getElementById('playPauseButton');
+let isPlaying = false;
+
+soundElements.forEach((element) => {
+    element.addEventListener('click', () => {
+        const soundId = element.getAttribute('data-sound');
+        const audioElement = document.getElementById(soundId);
+
+        if (audioElement.paused) {
+            audioElement.play();
+            element.classList.add('playing');
+        } else {
+            audioElement.pause();
+            audioElement.currentTime = 0;
+            element.classList.remove('playing');
+        }
+    });
+});
+
+playPauseButton.addEventListener('click', () => {
+    const audioElements = document.querySelectorAll('audio');
+
+    if (!isPlaying) {
+        audioElements.forEach((audioElement) => {
+            audioElement.play();
+        });
+        playPauseButton.textContent = 'Pause';
+    } else {
+        audioElements.forEach((audioElement) => {
+            audioElement.pause();
+            audioElement.currentTime = 0;
+        });
+        playPauseButton.textContent = 'Play';
+    }
+
+    isPlaying = !isPlaying;
+});
+
+
+
+
+
+
